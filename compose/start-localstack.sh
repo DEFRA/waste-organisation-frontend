c326e9ded872ed76f3bad8c17cd8321e1542099e
Ed Bowler <ed.bowler@equalexperts.com>
2026-01-12 10:51:42 +0000

    DWT-1401: add cdp uploader to local setup

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -9,3 +9,25 @@ export AWS_SECRET_ACCESS_KEY=test
 
 # SQS queues
 # aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name my-queue
+
+
+
+# Copied from cdp-uploader/compose/01-start-localstack.sh
+
+echo "[INIT SCRIPT] Creating buckets" >&2
+
+aws --endpoint-url=http://localhost:4566 s3 mb s3://cdp-uploader-quarantine
+aws --endpoint-url=http://localhost:4566 s3 mb s3://my-bucket
+
+echo "[INIT SCRIPT] Creating queues" >&2
+
+# queues
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-clamav-results
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo --attributes "{\"FifoQueue\":\"true\",\"ContentBasedDeduplication\": \"true\"}"
+
+# test harness
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name mock-clamav
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket cdp-uploader-quarantine --notification-configuration '{"QueueConfigurations": [{"QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav","Events": ["s3:ObjectCreated:*"]}]}'
+aws --endpoint-url=http://localhost:4566 sqs create-queue --region $AWS_REGION --queue-name cdp-uploader-download-requests
+
+# end of copypasta
