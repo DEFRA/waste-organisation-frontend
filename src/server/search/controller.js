aaaeac9a87b5cbd96b6e9b1a31df977086ff2e39
Philip Morris <philip@Philips-MacBook-Air.local>
2026-03-13 17:43:50 +0000

    list FF on /search

--- src/server/search/controller.js
+++ src/server/search/controller.js
@@ -2,11 +2,26 @@ import boom from '@hapi/boom'
 
 import { config } from '../../config/config.js'
 
+const featureFlagEnvVars = {
+  updateSpreadsheet: 'FEATURE_FLAG_UPDATE_SPREADSHEET',
+  accountPage: 'FEATURE_FLAG_ACCOUNT_PAGE',
+  searchPage: 'FEATURE_FLAG_SEARCH_PAGE',
+  signOut: 'FEATURE_FLAG_SIGN_OUT',
+  serviceCharge: 'FEATURE_FLAG_SERVICE_CHARGE'
+}
+
 export const searchController = {
   handler(request, h) {
     if (!config.get('featureFlags.searchPage')) {
       throw boom.notFound()
     }
+
+    const flags = config.get('featureFlags')
+    const featureFlags = Object.entries(flags).map(([name, enabled]) => ({
+      name,
+      envVar: featureFlagEnvVars[name],
+      enabled
+    }))
 
     request.contentSecurityPolicy = {
       extraAuthOrigins: request.auth.credentials.providerEndpoints
@@ -14,7 +29,8 @@ export const searchController = {
     return h.view('search/index', {
       pageTitle: 'Search',
       heading: 'You are logged in',
-      credentials: JSON.stringify(request.auth.credentials, null, 2)
+      credentials: JSON.stringify(request.auth.credentials, null, 2),
+      featureFlags
     })
   }
 }
