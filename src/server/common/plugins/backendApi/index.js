4f0f24c14cd71203a1dac9650f6b3a5d6fcdaad4
Philip J Smith <philip.j.smith@contractsquared.com>
2026-03-19 12:37:00 +0000

    Loged value of trace header

--- src/server/common/plugins/backendApi/index.js
+++ src/server/common/plugins/backendApi/index.js
@@ -22,6 +22,12 @@ const apiCall = async (asyncFunc, preSharedKey, url, payload) => {
 
     logger.debug(`request data: ${Object.keys(r.headers)}`)
 
+    if (r.headers[tracingHeader]) {
+      logger.debug(`request data - header: ${r.headers[tracingHeader]}`)
+    } else {
+      logger.debug(`request data - header: No tracingHeader set`)
+    }
+
     return response.payload
   } catch (e) {
     logger.error(`ERROR calling backend api ${e}, ${url}, ${payload}`)
