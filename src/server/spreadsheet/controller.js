41f067433b4584dcfe43356179619910b6240203
Phillip Smith <46485944+philjzmith@users.noreply.github.com>
2026-03-23 18:29:03 +0000

    added File upload id to file uploaded page (#95)

--- src/server/spreadsheet/controller.js
+++ src/server/spreadsheet/controller.js
@@ -6,7 +6,7 @@ import {
   initiateUpload,
   createCallbackHandler
 } from '../common/helpers/cdp-upload.js'
-
+const uploadSessionName = 'upload'
 const logger = createLogger()
 
 export const beginUpload = {
@@ -30,6 +30,10 @@ export const beginUpload = {
         uploadType: 'create'
       }
     )
+
+    request.yar.set(uploadSessionName, { uploadId })
+    await request.yar.commit(h)
+
     logger.info(`uploaded requested - ${uploadId} ${uploadUrl}`)
     const { origin } = new URL(
       uploadUrl?.startsWith('h') ? uploadUrl : config.get('fileUpload.url')
@@ -51,12 +55,12 @@ export const fileUploaded = {
   /* v8 ignore stop */
   async handler(request, h) {
     const organisationName = request?.auth?.credentials?.currentOrganisationName
-
     const pageContent = content.spreadsheetUploaded(request, organisationName)
-
+    const uploadData = request.yar.get(uploadSessionName)
     return h.view('spreadsheet/file-uploaded', {
       pageTitle: pageContent.heading.text,
       content: pageContent.content,
+      uploadData,
       returnAction: {
         text: pageContent.returnLink,
         link: paths.nextAction
