6b028b47fbc0daf3f0e7c0e36cbf475192f6a123
Philip Morris <philip@Philips-MacBook-Air.local>
2026-04-02 11:21:16 +0100

    re-add comment

--- src/server/health/controller.js
+++ src/server/health/controller.js
@@ -1,5 +1,8 @@
 import { statusCodes } from '../common/constants/status-codes.js'
 
+/**
+ * A generic health-check endpoint. Used by the platform to check if the service is up and handling requests.
+ */
 export const healthController = (_request, h) => {
   return h.response({ message: 'success' }).code(statusCodes.ok)
 }
