ad35825278d002fa5af39ca9939fa0358634de5b
Ed Bowler <ed.bowler@equalexperts.com>
2026-04-28 11:01:03 +0100

    DWT-2202: sonarcloud tidying

--- src/server/serviceCharge/paymentCallback/controller.js
+++ src/server/serviceCharge/paymentCallback/controller.js
@@ -1,11 +1,11 @@
 import boom from '@hapi/boom'
 import crypto from 'node:crypto'
 import { config } from '../../../config/config.js'
+import { statusCodes } from '../../common/constants/status-codes.js'
 
 export const paymentWebhookController = {
   async handler(request, h) {
     // TODO fix the logic when we've finished debugging
-    console.log('fish')
     const webhookSigningSecret = config.get('govPay.webhookSigningSecret')
     const webhookMessageBody = request.payload
     if (webhookMessageBody) {
@@ -13,26 +13,18 @@ export const paymentWebhookController = {
         .createHmac('sha256', webhookSigningSecret)
         .update(webhookMessageBody)
         .digest('hex')
-      console.log('fish 1')
 
       if (hmac === request.headers['pay-signature']) {
-        console.log('fish 2')
         try {
           const parsedMessage = JSON.parse(webhookMessageBody)
           request.logger.info(
             `webhookMessageBody: ${JSON.stringify(parsedMessage, null, 4)}`
           )
-          console.log('fish 3')
           request.backendApi.savePayment(
             parsedMessage.resource.metadata.organisationId,
             parsedMessage.resource
           )
-          console.log('fish 4')
         } catch (e) {
-          console.log(
-            'webhookMessageBody: ',
-            JSON.stringify(webhookMessageBody, null, 4)
-          )
           request.logger.error(
             `Error saving payment: ${e} message: ${webhookMessageBody} stacktrace: ${e.stack}`
           )
@@ -43,6 +35,6 @@ export const paymentWebhookController = {
     } else {
       request.logger.error(`No message body`)
     }
-    return h.response().code(200)
+    return h.response().code(statusCodes.ok)
   }
 }
