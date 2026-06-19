ecdfb14554a282f8ec34f20fa1fe5f3451c8997b
Philip J Smith <philip.j.smith@contractsquared.com>
2026-06-19 17:02:11 +0100

    removed comment

--- src/server/common/helpers/proxy/setup-proxy.js
+++ src/server/common/helpers/proxy/setup-proxy.js
@@ -7,11 +7,6 @@ import { config } from '../../../../config/config.js'
 
 const logger = createLogger()
 
-/**
- * If HTTP_PROXY is set setupProxy() will enable it globally
- * for a number of http clients.
- * Node Fetch will still need to pass a ProxyAgent in on each call.
- */
 export function setupProxy() {
   const proxyUrl = config.get('httpProxy')
 
