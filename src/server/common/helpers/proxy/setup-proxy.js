1a9bd75f6ce35f5a80c0d5e94703cb2b4df1b81a
Ed Bowler <ed.bowler@equalexperts.com>
2026-06-24 13:34:43 +0100

    DWT-2442: bump node version

--- src/server/common/helpers/proxy/setup-proxy.js
+++ src/server/common/helpers/proxy/setup-proxy.js
@@ -1,19 +1,17 @@
-import http from 'node:http'
-import https from 'node:https'
 import Wreck from '@hapi/wreck'
-
 import { createLogger } from '../logging/logger.js'
 import { config } from '../../../../config/config.js'
+import Https from 'node:https'
+import Http from 'node:http'
 
 const logger = createLogger()
 
-export function setupProxy() {
-  const proxyUrl = config.get('httpProxy')
-
-  if (proxyUrl) {
-    logger.info('setting up global proxies')
-
-    Wreck.agents.http = http.globalAgent
-    Wreck.agents.https = https.globalAgent
+export const setupProxy = () => {
+  if (config.get('httpProxy')) {
+    logger.info('Routing outbound requests via proxy')
+    // Required for Wreck
+    Wreck.agents.http = Http.globalAgent
+    Wreck.agents.https = Https.globalAgent
   }
+  return Wreck
 }
