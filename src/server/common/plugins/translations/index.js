ee0105578463ae369f6093840fd351a4512fdb3c
Philip J Smith <philip.j.smith@contractsquared.com>
2026-08-18 10:00:56 +0100

    removed locale setting

--- src/server/common/plugins/translations/index.js
+++ src/server/common/plugins/translations/index.js
@@ -18,16 +18,17 @@ export const translation = {
     name: 'translation',
     register: async (server) => {
       server.ext('onPreHandler', (request, h) => {
-        console.log(
-          'initial language header',
-          request.headers['accept-language']
-        )
+        // console.log(
+        //   'initial language header',
+        //   request.headers['accept-language']
+        // )
 
-        const browserLanguage = getBrowserLanguage(
-          request.headers['accept-language']
-        )
-        console.log('browser language')
-        request.locale = browserLanguage?.language === 'cy' ? 'zz' : 'en'
+        // const browserLanguage = getBrowserLanguage(
+        //   request.headers['accept-language']
+        // )
+        // console.log('browser language')
+        // request.locale = browserLanguage?.language === 'cy' ? 'zz' : 'en'
+        request.locale = 'en'
         return h.continue
       })
     }
